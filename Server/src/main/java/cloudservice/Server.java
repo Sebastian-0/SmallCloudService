6a7fb1e439a391bcfe203e4ec103f0382f313c12
Sebastian Hjelm <melon.bag@hotmail.com>
2021-06-05 18:21:27 +0200

    Initial setup

---
+++ Server/src/main/java/cloudservice/Server.java
@@ -0,0 +1,12 @@
+package cloudservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Server {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Server.class);
+
+	public static void main(String[] args) {
+		LOGGER.info("Welcome!");
+	}
+}
