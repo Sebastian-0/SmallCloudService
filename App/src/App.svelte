57111fbefd569f4b1daa10380374003b38dbe4dc
Sebastian Hjelm <melon.bag@hotmail.com>
2021-06-13 17:46:40 +0200

    Replace favicon

--- App/src/App.svelte
+++ App/src/App.svelte
@@ -9,9 +9,26 @@
 
 <ViewSynonyms/>
 
+<footer>
+	<div>Favicon made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+</footer>
+
 <style>
 	hr {
 		text-align: center;
 		width: 50%;
+	}
+
+	footer {
+		position: fixed;
+		bottom: 0;
+		left: 0;
+		right: 0;
+		font-size: smaller;
+		color: gray;
+	}
+
+	a {
+		color: gray;
 	}
 </style>
